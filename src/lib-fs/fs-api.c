dovecot/core

lib-fs: abort parent in fs_file_write_stream_abort_parent

src/lib-fs/fs-api.c
@@ -747,7 +747,7 @@ void fs_write_stream_abort_parent(struct fs_file *file, struct ostream **output)
 {
 	i_assert(file->parent != NULL);
 	i_assert(fs_file_last_error(file->parent) != NULL);
-	fs_write_stream_abort(file, output);
+	fs_write_stream_abort(file->parent, output);
 }
 
 void fs_write_set_hash(struct fs_file *file, const struct hash_method *method,

stephanbosch/dovecot-core

src/lib-fs/fs-api.c
@@ -738,7 +738,7 @@ void fs_write_stream_abort_parent(struct fs_file *file, struct ostream **output)
 {
 	i_assert(file->parent != NULL);
 	i_assert(fs_file_last_error(file->parent) != NULL);
-	fs_write_stream_abort_int(file, output);
+	fs_write_stream_abort_int(file->parent, output);
 }
 
 void fs_write_set_hash(struct fs_file *file, const struct hash_method *method,